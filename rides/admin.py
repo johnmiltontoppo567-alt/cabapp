cbc3cef5ebec8eb837d0c1d5fc9ddd259112b8af
johnmiltontoppo567-alt <johnmiltontoppo567@gmail.com>
2026-03-21 22:30:30 +0530

    feat: show driver info to passenger, implement forgot password flow, add email to admin

--- rides/admin.py
+++ rides/admin.py
@@ -11,13 +11,21 @@ mark_cancelled.short_description = "Mark selected rides as cancelled"
 
 class RideAdmin(admin.ModelAdmin):
     list_display = ['id', 'pickup', 'drop_location',
-                    'status', 'passenger', 'driver', 'created_at']
+                    'status', 'passenger', 'get_passenger_email', 'driver', 'get_driver_email', 'created_at']
     list_filter = ['status']
-    search_fields = ['passenger__username',
-                     'driver__username',
+    search_fields = ['passenger__username', 'passenger__email',
+                     'driver__username', 'driver__email',
                      'pickup', 'drop_location']
     ordering = ['-created_at']
     readonly_fields = ['passenger', 'driver', 'created_at', 'updated_at']
     actions = [mark_completed, mark_cancelled]
+
+    def get_passenger_email(self, obj):
+        return obj.passenger.email if obj.passenger else '-'
+    get_passenger_email.short_description = 'Passenger Email'
+
+    def get_driver_email(self, obj):
+        return obj.driver.email if obj.driver else '-'
+    get_driver_email.short_description = 'Driver Email'
 
 admin.site.register(Ride, RideAdmin)
--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'role', 'phone']
+    list_display = ['user', 'get_email', 'role', 'phone']
     list_filter = ['role']
-    search_fields = ['user__username']
+    search_fields = ['user__username', 'user__email']
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
 
 admin.site.register(UserProfile, UserProfileAdmin)
